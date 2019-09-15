d59629c220a2a9e63fd8972fd2ff4e0afbd491ee
communityus <37026425+communityus@users.noreply.github.com>
2019-09-14 23:35:15 -0600

    assets

--- docker/run_tle_nginx.sh
+++ docker/run_tle_nginx.sh
@@ -5,5 +5,6 @@ docker run -it -p ${TLE_SERVER_LISTEN:-127.0.0.1}:${TLE_SERVER_PORT:-8000}:80 	\
   --volumes-from tle-captcha-data       		\
   -v ${PWD}/../etc:/data/Lacuna-Server/etc 		\
   -v ${PWD}/../var:/data/Lacuna-Server/var 		\
+  -v ${PWD}/../assets:/data/Lacuna-Assets
   -d lacuna/tle-nginx
 
